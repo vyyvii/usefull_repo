7c8a5f76841173dd53fc84e4a2dd35dafc5ac8e1
Victor Defauchy <victor.defauchy@epitech.eu>
2026-02-12 18:23:30 +0100

    FIX: get_precision

--- Start/lib/src/int_str/get_precision.c
+++ Start/lib/src/int_str/get_precision.c
@@ -11,7 +11,7 @@
  * @ingroup int_str
  * @brief Give the precision of a double number
  * @param number String to test
- * @return Return the precision of the number, 0 if an error occurs
+ * @return Return the precision of the number, -1 if an error occurs
  * @note Complexity: O(n)
  * @note Part of UtilsLib by Victor Defauchy.
  */
@@ -20,13 +20,20 @@ int get_precision(char *number)
     int i = 0;
     int precision = 0;
 
-    while (number[i] != '.')
+    if (!number || !is_digit(number[0]))
+        return -1;
+    if (!contains_char(number, '.'))
+        return 0;
+    while (number[i] && number[i] != '.')
         i++;
+    i++;
+    if (!is_digit(number[i]))
+        return -1;
     while (number[i]) {
         if (is_digit(number[i]))
             precision++;
         else
-            return 0;
+            return -1;
         i++;
     }
     return precision;
